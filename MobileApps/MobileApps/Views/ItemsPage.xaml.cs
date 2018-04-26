4fef6631fa20f459bada062dd3bf2104069162b7
lruval <lruval@microsoft.com>
2018-04-26 15:56:29 -0600

    Initial commit

---
+++ MobileApps/MobileApps/Views/ItemsPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MobileApps.Models;
+using MobileApps.Views;
+using MobileApps.ViewModels;
+
+namespace MobileApps.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemsPage : ContentPage
+	{
+        BoletasViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new BoletasViewModel();
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var boleta = args.SelectedItem as Boleta;
+            if (boleta == null)
+                return;
+
+            await Navigation.PushAsync(new ItemDetailPage(new BoletaDetailViewModel(boleta)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Boletas.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
